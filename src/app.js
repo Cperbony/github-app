b5cf832d426384e7c4e17194f83216a91f2423f0
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-07-08 13:58:31 -0300

    Buscar repositorio do usuário pesquisado

--- src/app.js
+++ src/app.js
@@ -14,13 +14,19 @@ class App extends Component {
     }
   }
 
+  getGitHubApiUrl(username, type) {
+    const internalUser = username ? `${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users/${internalUser}${internalType}`
+  }
+
   handleSearch(e) {
     const value = e.target.value
     const keyCode = e.which || e.keyCode
     const ENTER = 13
 
     if (keyCode === ENTER) {
-      ajax().get(`https://api.github.com/users/${value}`)
+      ajax().get(this.getGitHubApiUrl(value))
         .then((result) => {
           this.setState({
             userinfo: {
@@ -30,10 +36,29 @@ class App extends Component {
               repos: result.public_repos,
               followers: result.followers,
               following: result.following
-            }
+            },
+            repos: [],
+            starred: []
           })
-          //console.log(result)
         })
+    }
+  }
+
+  getRepos(type) {
+    return (e) => {
+      const username = this.state.userinfo.login
+      console.log('Type: ', type)
+      ajax()
+        .get(this.getGitHubApiUrl(username, type))
+        .then((result) => {
+          this.setState({
+            [type]: result.map((repo) => ({
+              name: repo.name,
+              link: repo.html_url
+            }))
+          })
+        })
+
     }
   }
 
@@ -43,6 +68,8 @@ class App extends Component {
       repos={this.state.repos}
       starred={this.state.starred}
       handleSearch={(e) => this.handleSearch(e)}
+      getRepos={this.getRepos('repos')}
+      getStarred={this.getRepos('starred')}
     />
   }
 }
