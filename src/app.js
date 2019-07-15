6c7719c26df45cae81e578efae41e76a7f3ee163
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-07-15 08:51:30 -0300

    Atualização repos

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ class App extends Component {
       starred: [],
       isFetching: false
     }
+    //Passar o this no construtor, para chamar o this no render
+    this.handleSearch = this.handleSearch.bind(this)
   }
 
   getGitHubApiUrl(username, type) {
@@ -71,11 +73,12 @@ class App extends Component {
 
   render() {
     return <AppContent
-      userinfo={this.state.userinfo}
-      repos={this.state.repos}
-      starred={this.state.starred}
-      isFetching={this.state.isFetching}
-      handleSearch={(e) => this.handleSearch(e)}
+      {...this.state}
+      // userinfo={this.state.userinfo}
+      // repos={this.state.repos}
+      // starred={this.state.starred}
+      // isFetching={this.state.isFetching}
+      handleSearch={this.handleSearch}
       getRepos={this.getRepos('repos')}
       getStarred={this.getRepos('starred')}
     />
